e4018aecefd780f32e07a465f4165f9ed4e2683b
nahuelf <nahue@hush.com>
2014-03-27 13:36:50 -0300

    framework css

---
+++ templates/home.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Eventos | CualPinta.com.ar</title>
+	<link rel="stylesheet" href="../css/empresa.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+		<nav id="nav">
+			<div class="container clearfix">
+				<div id="logo">
+					<div id="logo-img"><img src="../imagenes/logo-bar.png" alt=""></div>
+				</div>
+				<div id="nav-bar" class="clearfix">
+					<ul id="nav-list">
+						<li class="nav-links"><a href="">Home</a></li>
+						<li class="nav-links"><a href="">Eventos</a></li>
+						<li class="nav-links"><a href="">Promociones</a></li>
+						<li class="nav-links"><a href="">Estadisticas</a></li>
+						<li class="nav-links"><a href="">Pricing</a></li>
+						<li class="nav-links"><a href="">Soporte</a></li>
+					</ul>
+				</div>
+				<div id="user-status">
+					<div id="img-avatar"><img src="../imagenes/img.png" alt=""></div>
+					<div id="avatar-nav-bar"><a href="">Nahuel</a></div>
+					<div class="profile-button"><a href="">Perfil</a></div>
+					<div class="logout-button"><a href="">Salir</a></div>
+				</div>
+			</div>
+		</nav>
+		<section id="content" class="container">
+			<h2 class="title">Home</h2>
+			<div class="row clearfix">
+				<article class="column twelve">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, ipsa consequuntur quos sint aperiam nemo! Consequatur, eaque, perspiciatis delectus dicta asperiores corporis quo. Voluptas, modi, iusto, distinctio, aliquid quaerat ipsum minima harum debitis pariatur optio natus asperiores dolore cumque ducimus alias temporibus magnam soluta quod quidem quasi veniam sit expedita! Quae, perferendis, fugit, molestias fuga nostrum distinctio blanditiis temporibus nesciunt accusamus tenetur fugiat necessitatibus quos velit quam ab a ea exercitationem voluptatum voluptates enim at maiores minima optio in dolorum impedit laborum saepe nam quas doloremque! Reiciendis, recusandae, voluptatem, quos obcaecati doloribus at nulla animi expedita maxime assumenda in minima.
+				</article>
+			</div>
+
+			<div class="row clearfix">
+				<article class="column four">
+					<h3 class="title">Eventos</h3>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, obcaecati, maiores rerum voluptatibus reprehenderit tempora cumque doloremque iusto repudiandae adipisci. Explicabo, omnis error culpa laudantium tempora nostrum beatae aut porro a id quisquam harum nemo eius ratione suscipit? Placeat voluptatibus beatae ex neque sint animi vero aliquid modi sunt voluptatum.
+					<button class="button medium">Crear evento</button>
+				</article>
+				<article class="column four">
+					<h3 class="title">Promos</h3>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis voluptatum laboriosam optio unde autem iste iusto deleniti fugit maiores quis! Ipsam, quo voluptatibus eveniet dolore magni odit voluptatum omnis veritatis quos iure perspiciatis obcaecati atque officia voluptates qui eum optio error accusamus eligendi ea laborum ullam sapiente aliquid reprehenderit rem.
+					<button class="button medium">Crear promo</button>
+				</article>
+				<article class="column four">
+					<h3 class="title">Estadisticas</h3>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, unde, reprehenderit ipsum veritatis a vel vero molestias voluptatibus accusamus molestiae assumenda temporibus dolorum corrupti fugiat odio quaerat numquam odit ullam sit ad nihil deserunt eum! Vel, numquam quod velit earum nobis? Doloremque, id qui numquam assumenda totam esse fuga sapiente.
+					<button class="button medium">Ver estadisticas</button>
+				</article>
+			</div>
+
+			<div class="row clearfix">
+				<article class="column twelve">
+				<table class="tab-info">
+					<thead>
+						<tr>
+							<th>Nombre</th>
+							<th>Tipo</th>
+							<th>Fecha</th>
+							<th>Lugar</th>
+							<th>Promocion</th>
+							<th>Seleccion</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Fiesta</td>
+							<td>Recital</td>
+							<td>15/2/2015</td>
+							<td>Parque Centenario</td>
+							<td>No tiene</td>
+							<td><input type="checkbox"></input></td>
+						</tr>
+						<tr>
+							<td>Fiesta</td>
+							<td>Recital</td>
+							<td>15/2/2015</td>
+							<td>Parque Centenario</td>
+							<td>No tiene</td>
+							<td><input type="checkbox"></input></td>
+						</tr>
+						<tr>
+							<td>Fiesta</td>
+							<td>Recital</td>
+							<td>15/2/2015</td>
+							<td>Parque Centenario</td>
+							<td>No tiene</td>
+							<td><input type="checkbox"></input></td>
+						</tr>
+						<tr>
+							<td>Fiesta</td>
+							<td>Recital</td>
+							<td>15/2/2015</td>
+							<td>Parque Centenario</td>
+							<td>No tiene</td>
+							<td><input type="checkbox"></input></td>
+						</tr>
+						<tr>
+							<td>Fiesta</td>
+							<td>Recital</td>
+							<td>15/2/2015</td>
+							<td>Parque Centenario</td>
+							<td>No tiene</td>
+							<td><input type="checkbox"></input></td>
+						</tr>
+					</tbody>
+				</table>
+			</article>
+			</div>
+		</section>
+
+		<footer id="footer">CualPinta 2014</footer>
+</body>
+</html>
